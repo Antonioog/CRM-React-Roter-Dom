56be795326643c6d8ba4ac81fa88cb0f51daafdb
Antonioog <arkanamos1985@gmail.com>
2023-07-13 00:53:46 -0600

    Creamos la api que vamos a utilizar en el proyecto

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -24,7 +24,7 @@ const NuevoCliente = () => {
 
                     <input
                         type="submit"
-                        className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+                        className="mt-5 w-full bg-blue-700 p-3 uppercase font-bold text-white text-lg"
                         value='Registrar Cliente'
                     />
                 </form >
