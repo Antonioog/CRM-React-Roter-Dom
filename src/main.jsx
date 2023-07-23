5862678eb9c835934e1ee2c92e892c2fcd7add5d
Antonioog <arkanamos1985@gmail.com>
2023-07-22 21:49:53 -0600

    Configuramos el el main.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './components/Layout'
-import NuevoCliente from './pages/NuevoCliente'
-import Index, { loader as clientesLoader } from './pages/Index'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Index />,
-        loader: clientesLoader
-      },
-      {
-        path: '/clientes/nuevo',
-        element: <NuevoCliente />
-      }
-    ]
+    element: <h1>Inicio</h1>
   },
-
+  {
+    path: '/nosotros',
+    element: <h1>Nosotros</h1>
+  }
 ])
+
 
 
 
