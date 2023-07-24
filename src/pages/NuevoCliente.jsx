4a4c07485c33bbe84389be1b1e865bf9d1a8d90a
Antonioog <arkanamos1985@gmail.com>
2023-07-24 01:11:17 -0600

    Creamos el boton de volver al home utilizando el hook useNaviagte

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom"
 
 function NuevoCliente() {
+    const navigate = useNavigate();
     return (
-        <div>Nuevo Cliente</div>
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+            <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
+            <div className="flex justify-end">
+                <button
+                    className="bg-blue-800 text-white px-3 py-1 font-bold uppercase hover:bg-blue-900"
+                    onClick={() => navigate('/')}
+                >Volver</button>
+            </div>
+
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-5">
+                <p>Formulario aqui</p>
+            </div>
+        </>
     )
 }
 
