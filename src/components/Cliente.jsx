86e20459d868443fbe22da06f9f8f7326a1e7b1d
Antonioog <arkanamos1985@gmail.com>
2023-08-02 22:09:11 -0600

    Obtuvimos el cliente mediate un id para modificarlo

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,6 +1,8 @@
-
+import { useNavigate } from "react-router-dom"
 
 function Cliente({ cliente }) {
+
+    const navigate = useNavigate()
     const { nombre, empresa, email, telefono, id } = cliente
     return (
         <>
@@ -16,7 +18,12 @@ function Cliente({ cliente }) {
                 </td>
 
                 <td className="p-6 flex gap-3">
-                    <button className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs">Editar</button>
+                    <button
+                        type="button"
+                        className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                        onClick={() => navigate(`/clientes/${id}/editar`)}
+                    >
+                        Editar</button>
                     <button className="text-red-600 hover:text-red-700 uppercase font-bold text-xs">Eliminar</button>
                 </td>
             </tr>
